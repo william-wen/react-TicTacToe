f29d78a70d5c7b9e3b7b28af75608bff1b21eae6
William Wen <wenwi@utschools.ca>
2019-06-20 12:11:34 +0800

    TicTacToe Upload

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Game from './Game.js';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+let age = 32;
+//the props object is automatically created when data is passed as attributes
+ReactDOM.render(<Game />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
